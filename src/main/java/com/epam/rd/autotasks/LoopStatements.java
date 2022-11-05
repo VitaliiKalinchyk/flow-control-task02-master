645d19360a23fe7878a01cc4d920dcf2437db4fe
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-05 18:44:37 +0200

    5 11

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -3,7 +3,9 @@ package com.epam.rd.autotasks;
 
 class LoopStatements {
     public static int sumOfBinary(int n) {
-       //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+        return Integer.bitCount(n);
     }
 }
